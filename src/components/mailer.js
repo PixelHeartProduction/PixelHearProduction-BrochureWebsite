7851aac8e52e1d7ec30a0d60f029f5af6dc40fe8
Taishoo <53165505+Taishoo@users.noreply.github.com>
2019-11-09 18:13:36 +0800

    Finished brochure version1

--- src/components/mailer.js
+++ src/components/mailer.js
@@ -1,42 +1,215 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { ToastContainer, toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import firebase from "firebase"
 
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `useStaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCheck, faExclamation } from "@fortawesome/free-solid-svg-icons"
 
-const Mailer = () => {
-  const container = {
-    backgroundColor: "teal",
+var firebaseConfig = {
+  apiKey: "AIzaSyA3wHtlQll7NBIrwJzw7yivcKdezUbe90g",
+  authDomain: "pixel-heart-production-mails.firebaseapp.com",
+  databaseURL: "https://pixel-heart-production-mails.firebaseio.com",
+  projectId: "pixel-heart-production-mails",
+  storageBucket: "pixel-heart-production-mails.appspot.com",
+  messagingSenderId: "815179189670",
+  appId: "1:815179189670:web:4bf32f435e221a5087d367",
+  measurementId: "G-8HRC3N4N2Z",
+}
+// Initialize Firebases
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
+const db = firebase.firestore()
+
+class Mailer extends React.Component {
+  notifyB = message =>
+    toast(message, {
+      containerId: "B",
+    })
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: "",
+      email: "",
+      title: "",
+      message: "",
+      date: "",
+      isDisabled: false,
+    }
   }
-  const text = {
-    color: "white",
-    marginBottom: 0,
+  componentDidMount() {
+    console.log(db)
+    const today = new Date()
+    let date =
+      today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate()
+    this.setState({ date: date })
   }
-  return (
-    <div style={container}>
-      <div>
-        <p style={text}>Name:</p>
-        <input type="text" placeholder="Name" />
+  handleSubmit = () => {
+    const { name, email, message, title } = this.state
+    const successStyle = {
+      display: "flex",
+      flexDirection: "row",
+      marginBottom: -20,
+    }
+    const messageStyle = {
+      marginLeft: 15,
+    }
+    if (name !== "" && email !== "" && message !== "" && title !== "") {
+      this.notifyB(
+        <div style={successStyle}>
+          <FontAwesomeIcon icon={faCheck} color="green" />
+          <h4 style={messageStyle}> Your message was sent!</h4>
+        </div>
+      )
+      fetch("http://api.ipify.org/?format=json")
+        .then(results => results.json())
+        .then(data => {
+          if (data !== undefined) {
+            this.clearTextInputs(data.ip)
+          }
+        })
+    } else {
+      this.notifyB(
+        <div style={successStyle}>
+          <FontAwesomeIcon icon={faExclamation} color="orange" />
+          <h4 style={messageStyle}> Fill all fields!!</h4>
+        </div>
+      )
+    }
+  }
+  handleInputChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+  clearTextInputs = ip => {
+    const { name, email, message, date } = this.state
+    db.collection("messages").add({
+      date,
+      email,
+      name,
+      locationIP: ip,
+      message,
+    })
+    this.setState({ isDisabled: true })
+    setTimeout(function() {
+      window.location.reload()
+    }, 2000)
+  }
+  render() {
+    const styles = {
+      container: {
+        padding: 15,
+        backgroundColor: "gray",
+        borderRadius: 5,
+        boxShadow: "3px 3px 3px gray",
+      },
+      text: {
+        color: "white",
+        marginBottom: 0,
+      },
+      textInput: {
+        display: "flex",
+        border: "none",
+        borderRadius: 1,
+      },
+      textInputLong: {
+        height: 200,
+        display: "flex",
+        flex: 1,
+        borderRadius: 1,
+        width: "100%",
+      },
+      button: {
+        marginTop: 10,
+        borderRadius: 5,
+        width: 125,
+        height: 35,
+      },
+      twoColumn: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+      },
+    }
+    return (
+      <div style={styles.container}>
+        <ToastContainer
+          enableMultiContainer
+          containerId={"B"}
+          position={toast.POSITION.TOP_RIGHT}
+          autoClose={3000}
+          hideProgressBar={true}
+        />
+        <form>
+          <div style={styles.twoColumn}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                flex: 1,
+                marginRight: 10,
+              }}
+            >
+              <p style={styles.text}>Name:</p>
+              <input
+                style={styles.textInput}
+                type="text"
+                placeholder="Name"
+                name="name"
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                flex: 1,
+                marginLeft: 10,
+              }}
+            >
+              <p style={styles.text}>Email:</p>
+              <input
+                style={styles.textInput}
+                type="text"
+                placeholder="Email"
+                name="email"
+                onChange={this.handleInputChange}
+              />
+            </div>
+          </div>
+
+          <div style={{ display: "flex", flexDirection: "column", flex: 1 }}>
+            <p style={styles.text}>Title:</p>
+            <input
+              style={styles.textInput}
+              type="text"
+              placeholder="Title"
+              name="title"
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <div>
+            <p style={styles.text}>Message:</p>
+            <textarea
+              height={220}
+              style={styles.textInputLong}
+              placeholder="Message"
+              name="message"
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <input
+            style={styles.button}
+            type="button"
+            disabled={this.state.isDisabled}
+            value="Send"
+            onClick={() => {
+              this.handleSubmit()
+            }}
+          />
+        </form>
       </div>
-      <div>
-        <p style={text}>Email:</p>
-        <input type="text" placeholder="Email" />
-      </div>
-      <div>
-        <p style={text}>Message:</p>
-        <textarea placeholder="Message" />
-      </div>
-      <input type="button" value="send" />
-    </div>
-  )
+    )
+  }
 }
 
 export default Mailer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 import "animate.css/animate.min.css"
+import { toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -11,115 +13,172 @@ import Kim from "../images/Kim.png"
 import Justine from "../images/Justine.png"
 import Lester from "../images/Lester.png"
 
-const IndexPage = () => {
-  const nameText = {
-    color: "orange",
-    marginTop: -20,
-  }
-  const roleText = {
-    color: "gray",
-    marginTop: -20,
-  }
-  const headerTextStyle = {
-    fontFamily: "Arial",
-    alignItems: "center",
-    textAlign: "center",
-  }
-  const pixelText = {
-    color: "orange",
-  }
-  const profPic = {
-    maxHeight: 200,
-    maxWidth: 200,
-  }
-  const teamBackground = {
-    display: "flex",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-    paddingLeft: 50,
-    paddingRight: 50,
-    paddingBottom: 50,
-    marginBottom: 25,
-  }
-  const teamTextHeader = {
-    color: "orange",
-  }
-  const teamProfile = {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-  }
-  return (
-    <div>
-      <Layout>
-        <SEO title="Home" />
-        <div>
-          <img src={wallpaper} />
-          <div style={headerTextStyle}>
-            <hr />
-            <h1 class="textHeader">
-              Changing the world one <span style={pixelText}>Pixel</span> at a
-              time.
-            </h1>
-            <hr />
-          </div>
-          <div>
-            <p>
-              Hi!, we are the
-              <span style={pixelText}> Pixel Heart Production</span> a small dev
-              team from Bulacan State University. As you might have expected we
-              really love video games and creating them is not an exception. We
-              specialize in 2D games and building assets (Graphics, Animations,
-              Audio, Scripts/Programs) we also do our own research and screen
-              writing and documenting it. To this end, we're doing out best to
-              revolutionize 2D gaming with modern technologies and IT knowledge
-              one pixel at a time.
-            </p>
-          </div>
-          <h2 style={teamTextHeader}>The Team:</h2>
-        </div>
-      </Layout>
-      <div style={teamBackground}>
-        <div style={teamProfile}>
-          <img src={Albert} style={profPic} />
-          <h4 style={nameText}>Albert De leon Jr.</h4>
-          <p style={roleText}>Team Leader/Developer</p>
-        </div>
-        <div style={teamProfile}>
-          <img src={Kim} style={profPic} />
-          <h4 style={nameText}>Kim Farley Arellano</h4>
-          <p style={roleText}>Designer/Developer</p>
-        </div>
-        <div style={teamProfile}>
-          <img src={Justine} style={profPic} />
-          <h4 style={nameText}>Justine Allen P. Manuel</h4>
-          <p style={roleText}>Designer/Documenter</p>
-        </div>
-        <div style={teamProfile}>
-          <img src={Lester} style={profPic} />
-          <h4 style={nameText}>Mark Lester Hermano</h4>
-          <p style={roleText}>Developer</p>
-        </div>
-      </div>
+import facebook from "../images/facebook-logo.png"
+import twitter from "../images/twitter-social-logotype.png"
+import gmail from "../images/gmail.png"
+import phone from "../images/smartphone.png"
 
+import ScrollAnimation from "react-animate-on-scroll"
+import "animate.css/animate.min.css"
+
+class IndexPage extends React.Component {
+  notify = () => toast("Wow so easy !")
+  render() {
+    const styles = {
+      nameText: { color: "orange", marginTop: -20 },
+      roleText: { color: "gray", marginTop: -20 },
+      headerTextStyle: {
+        fontFamily: "Arial",
+        alignItems: "center",
+        textAlign: "center",
+      },
+      pixelText: { color: "orange" },
+      profPic: { maxHeight: 200, maxWidth: 200 },
+      teamBackground: {
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "space-around",
+        paddingLeft: 50,
+        paddingRight: 50,
+        paddingBottom: 50,
+        marginBottom: 25,
+      },
+      teamTextHeader: {
+        color: "orange",
+      },
+      teamProfile: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      },
+      contactIcons: {
+        width: 25,
+        height: 25,
+      },
+      contactContainer: {
+        display: "flex",
+        flexDirection: "row",
+        minWidth: 250,
+      },
+    }
+    return (
       <div>
         <Layout>
-          <hr />
-          <h2 style={teamTextHeader}>Contact us:</h2>
+          <SEO title="Home" />
           <div>
-            <Mailer />
+            <img src={wallpaper} />
+            <div style={styles.headerTextStyle}>
+              <hr />
+              <h1 className="textHeader">
+                Changing the world one{" "}
+                <span style={styles.pixelText}>Pixel</span> at a time.
+              </h1>
+              <hr />
+            </div>
+            <div>
+              <p>
+                Hi!, we are the
+                <span style={styles.pixelText}> Pixel Heart Production</span> a
+                small dev team from Bulacan State University. As you might have
+                expected we really love video games and creating them is not an
+                exception. We specialize in 2D games and building assets
+                (Graphics, Animations, Audio, Scripts/Programs) we also do our
+                own research and screen writing and documenting it. To this end,
+                we're doing out best to revolutionize 2D gaming with modern
+                technologies and IT knowledge one pixel at a time.
+              </p>
+            </div>
+            <h2 style={styles.teamTextHeader}>The Team:</h2>
           </div>
         </Layout>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <ScrollAnimation
+          animateIn="fadeIn"
+          initiallyVisible={true}
+          duration={3}
+        >
+          <div style={styles.teamBackground}>
+            <div style={styles.teamProfile}>
+              <img src={Albert} style={styles.profPic} />
+              <h4 style={styles.nameText}>Albert De leon Jr.</h4>
+              <p style={styles.roleText}>Team Leader/Developer</p>
+            </div>
+            <div style={styles.teamProfile}>
+              <img src={Kim} style={styles.profPic} />
+              <h4 style={styles.nameText}>Kim Farley Arellano</h4>
+              <p style={styles.roleText}>Designer/Developer</p>
+            </div>
+            <div style={styles.teamProfile}>
+              <img src={Justine} style={styles.profPic} />
+              <h4 style={styles.nameText}>Justine Allen P. Manuel</h4>
+              <p style={styles.roleText}>Designer/Documenter</p>
+            </div>
+            <div style={styles.teamProfile}>
+              <img src={Lester} style={styles.profPic} />
+              <h4 style={styles.nameText}>Mark Lester Hermano</h4>
+              <p style={styles.roleText}>Developer</p>
+            </div>
+          </div>
+        </ScrollAnimation>
+
+        <div>
+          <Layout>
+            <hr />
+            <h2 style={styles.teamTextHeader}>Contact us:</h2>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                flexWrap: "wrap",
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flex: 1,
+                  flexDirection: "column",
+                }}
+              >
+                <div style={styles.contactContainer}>
+                  <img src={facebook} style={styles.contactIcons} />
+                  <p style={{ marginLeft: 7 }}> Coming soon.</p>
+                </div>
+                <div style={styles.contactContainer}>
+                  <img src={twitter} style={styles.contactIcons} />
+                  <p style={{ marginLeft: 7 }}>Coming soon.</p>
+                </div>
+              </div>
+              <div
+                style={{ display: "flex", flex: 1, flexDirection: "column" }}
+              >
+                <div style={styles.contactContainer}>
+                  <img src={phone} style={styles.contactIcons} />
+                  <p style={{ marginLeft: 7 }}> 0945-351-6391</p>
+                </div>
+                <div style={styles.contactContainer}>
+                  <img src={gmail} style={styles.contactIcons} />
+                  <p style={{ marginLeft: 7 }}>
+                    PixelHeartProductionGaming@gmail.com
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <Mailer />
+            </div>
+          </Layout>
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default IndexPage
